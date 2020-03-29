34ceb137a43b95c6b79e6fee3e3d5def38c0f460
damorenon <damorenon@gmail.com>
2020-03-29 15:29:05 -0500

    adding initial stuff

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./AuthorQuiz.css";
+import "./bootstrap.min.css";
+
+class AuthorQuiz extends Component {
+  render() {
+    return <div>AuthorQuiz APP</div>;
+  }
+}
+
+export default AuthorQuiz;
