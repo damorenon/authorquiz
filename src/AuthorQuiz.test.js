8fee5c034aa9bb53bcea6a581eaeb1c1b01fa4ed
damorenon <damorenon@gmail.com>
2020-03-29 18:28:03 -0500

    JSX module, adding authors data and components

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDom from "react-dom";
 import AuthorQuiz from "./AuthorQuiz";
 
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The adventures of Huckleberry Finn",
+      "life on the Mississippi",
+      "Roughing It"
+    ]
+  }
+];
+
+const state = {
+  turnData: {
+    author: authors[0],
+    books: authors[0].books
+  }
+};
+
 describe("Author Quiz", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDom.render(<AuthorQuiz />, div);
+    ReactDom.render(<AuthorQuiz {...state} />, div);
   });
 });
--- src/index.js
+++ src/index.js
@@ -4,12 +4,80 @@ import "./index.css";
 import AuthorQuiz from "./AuthorQuiz";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <AuthorQuiz />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const authors = [
+  {
+    name: "Mark Twain",
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: [
+      "The Adventures of Huckleberry Finn",
+      "Tom Sawyer",
+      "Life on the Mississsippi",
+      "Roughing It",
+      "A Connecticut Yankee in King Arthurs Court"
+    ]
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/josephconrad.png",
+    imageSource: "Wikimedia Commons",
+    books: ["Heart of Darkness"]
+  },
+  {
+    name: "J.K. Rowling",
+    imageUrl: "images/authors/jkrowling.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Daniel Ogren",
+    books: ["Harry Potter and the Sorcerers Stone"]
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    imageAttribution: "Pinguino",
+    books: ["The Shining", "IT"]
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/charlesdickens.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["David Copperfield", "A Tale of Two Cities"]
+  },
+  {
+    name: "William Shakespeare",
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ["Hamlet", "Macbeth", "Romeo and Juliet"]
+  }
+];
+
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+function getTurnData(authors) {
+  const allBooks = authors.reduce((p, c, i) => p.concat(c.books), []);
+  const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+  const answer =
+    fourRandomBooks[Math.floor(Math.random() * fourRandomBooks.length)];
+  return {
+    books: fourRandomBooks,
+    author: authors.find(author => author.books.some(title => title === answer))
+  };
+}
+
+const state = {
+  turnData: getTurnData(authors)
+};
+
+ReactDOM.render(<AuthorQuiz {...state} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
